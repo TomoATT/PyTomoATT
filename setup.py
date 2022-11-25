79e1dc25b563586cf6f49bc0118c0f8303613e28
xumi1993 <gomijianxu@gmail.com>
2022-11-25 18:42:57 +0800

    add ATTData

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-import numpy
-setup(ext_modules = cythonize(Extension(
-    'src_rec',
-    sources=['src/src_rec.pyx'],
-    language='c',
-    include_dirs=[numpy.get_include()],
-    library_dirs=[],
-    libraries=[],
-    extra_compile_args=[],
-    extra_link_args=[]
-)))
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+packages = find_packages()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+VERSION = "0.1.0"
+setup(name='PyTomoATT',
+      version=VERSION,
+      author='Mijian Xu, Masaru Nagaso',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author_email='mijian.xu@ntu.edu.sg',
+      license='GPLv3',
+      package_dir={'pytomoatt': 'pytomoatt'},
+      packages=find_packages(),
+      install_requires=[
+                'numpy>=1.19.0',
+                'scipy>=1.9.1',
+                'pandas>=1.4.0',
+                'h5py',
+                'pyyaml',
+                'xarray'],
+      zip_safe=False,
+      classifiers=['Programming Language :: Python',
+                   'Programming Language :: Python :: 3.9',
+                   'Programming Language :: Python :: 3.10',
+                   'Programming Language :: Python :: 3.11']
+      )
