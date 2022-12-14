186f746e9f6a56966d30d64d41623f4c0857434f
Masaru Nagaso <mnsaru22@gmail.com>
2022-12-14 19:19:06 +0800

    support general format of srcrec file (with weight and dist_deg)

--- pytomoatt/src_rec.py
+++ pytomoatt/src_rec.py
@@ -14,23 +14,43 @@ class SrcRec():
         self.src_only = src_only
         self.src_points = None
         self.rec_points = None
-    
+
+    def __repr__(self):
+        return f"PyTomoATT SrcRec Object: \n\
+                fname={self.fname}, \n\
+                src_only={self.src_only}, \n\
+                number of sources={self.src_points.shape[0]}, \n\
+                number of receivers={self.rec_points.shape[0]}"
+
     @classmethod
-    def read(cls, fname:str, **kwargs):
+    def read(cls, fname: str, dist_in_data=False, **kwargs):
         """Read source <--> receiver file to pandas.DataFrame
 
         :param fname: Path to src_rec file
         :type fname: str
+        :param dist_in_data: Whether distance is included in the src_rec file
+        :type dist_in_data: bool
         :return: class of SrcRec
         :rtype: SrcRec
         """
         sr = cls(fname, **kwargs)
-        alldf = pd.read_table(fname, sep='\s+|\t', engine='python', header=None)
-        sr.src_points = alldf[pd.notna(alldf[13])]
-        sr.src_points.index = sr.src_points[0]
-        if sr.src_points.shape[1] == 13:
-            sr.src_points = pd.concat([sr.src_points, pd.Series([1.0]*sr.src_points.shape[0])])
-        datedf = sr.src_points[sr.src_points.columns[1:7]]
+        alldf = pd.read_table(fname, sep='\s+|\t', engine='python',
+                              header=None, comment="#")
+
+        last_col = 12
+        # this is a source line if the last column is not NaN
+        sr.src_points = alldf[pd.notna(alldf[last_col])]
+        # add weight column if not included
+        if sr.src_points.shape[1] == last_col+1:
+            # add another column for weight
+            sr.src_points.loc[:, last_col+1] = 1.0
+
+        # src id dataframe
+        sr.src_points.index = sr.src_points.iloc[:, 0]
+        sr.src_points.index.name = 'src_index'
+
+        # event datetime dataframe
+        datedf = sr.src_points.loc[:, 1:6]
         type_dict = {
             1: int,
             2: int,
@@ -39,14 +59,19 @@ class SrcRec():
             5: int,
             6: float,
         }
-        datedf = datedf.astype(type_dict)
+        try:
+            datedf = datedf.astype(type_dict)
+        except:
+            print("Error: please check the date format in the src_rec file")
+            return sr.src_points
         dateseris = datedf.astype(str).apply(lambda x: '.'.join(x), axis=1).apply(
             pd.to_datetime, format='%Y.%m.%d.%H.%M.%S.%f')
         dateseris.name = 'origin_time'
-        src_data = sr.src_points[sr.src_points.columns[7:]]
+        # event data dataframe
+        src_data = sr.src_points.loc[:, 7:]
         src_data.columns = ['evla', 'evlo',
-                            'evdp', 'mag', 'num_rec', 
-                            'event_id', 'weight',]
+                            'evdp', 'mag', 'num_rec',
+                            'event_id', 'weight']
         type_dict = {
             'evla': float,
             'evlo': float,
@@ -56,16 +81,54 @@ class SrcRec():
             'event_id': str,
             'weight': float
         }
-        src_data = src_data.astype(type_dict)
+        try:
+            src_data = src_data.astype(type_dict)
+        except:
+            print("Error2: please check the event data format in the src_rec file")
+            return sr.src_points
+
+        # concat all the 3 dataframes
         sr.src_points = pd.concat([dateseris, src_data], axis=1)
+
+        # read receiver data if not src_only
         if not sr.src_only:
-            sr.rec_points = alldf[pd.isna(alldf[8])].reset_index(drop=True)
-            sr.rec_points = sr.rec_points[sr.rec_points.columns[0:8]]
-            sr.rec_points.columns = [
-                'src_index', 'rec_index', 'staname', 'stla', 'stlo', 'stel', 'phase', 'tt'
-            ]
+            # number of columns is 8 if distance is not included
+            if not dist_in_data:
+                last_col = 7
+            else:
+                last_col = 8
+
+            # extract the rows if the last_col is not NaN and the 12th column is NaN
+            sr.rec_points = alldf[(alldf[last_col].notna())
+                                & (alldf[12].isna())].reset_index(drop=True)
+
+            # add weight column if not included
+            if sr.rec_points.loc[:, last_col+1].isna().all():
+                sr.rec_points.loc[:, last_col+1] = 1.0
+
+            # warning if weigh value is greater than 10
+            if (sr.rec_points.loc[:, last_col+1] > 10).any():
+                print("""
+Warning: at least one weight value is greater than 10.
+Probably your src_rec file includes distance data.
+In this case, please set dist_in_data=True and read again.""")
+
+            # extract only the first part of columns (cut unnecessary columns)
+            sr.rec_points = sr.rec_points.loc[:, :last_col+1]
+
+            if not dist_in_data:
+                sr.rec_points.columns = [
+                    'src_index', 'rec_index', 'staname',
+                    'stla', 'stlo', 'stel', 'phase', 'tt', 'weight'
+                ]
+            else:
+                sr.rec_points.columns = [
+                    'src_index', 'rec_index', 'staname',
+                    'stla', 'stlo', 'stel', 'phase', 'dist_deg', 'tt', 'weight'
+                ]
+
         return sr
-    
+
     def write(self, fname='src_rec_file'):
         """Write sources and receivers to ASCII file for TomoATT
 
@@ -77,7 +140,7 @@ class SrcRec():
                 time_lst = src['origin_time'].strftime('%Y_%m_%d_%H_%M_%S.%f').split('_')
                 f.write('{:d} {} {} {} {} {} {} {:.4f} {:.4f} {:.4f} {:.4f} {} {} {:.4f}\n'.format(
                     idx, *time_lst, src['evla'], src['evlo'], src['evdp'],
-                    src['mag'], src['num_rec'], src['event_id'], src['weight'] 
+                    src['mag'], src['num_rec'], src['event_id'], src['weight']
                 ))
                 if self.src_only:
                     continue
